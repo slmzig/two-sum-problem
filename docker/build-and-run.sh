647b8d2e007021205ea83fa5a976cf6804e047e4
Slava <slmzig@gmail.com>
2025-04-15 13:02:13 +0300

    fix error in building script

--- docker/build-and-run.sh
+++ docker/build-and-run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-set -e
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
+cd "$(dirname "$0")/.."
 
 sbt clean
+
 sbt docker:publishLocal
 
-sh "$SCRIPT_DIR/docker-down.sh"
-sh "$SCRIPT_DIR/docker-up.sh"
+docker compose -f docker/docker-compose.yaml down
+
+# run as a daemon
+docker compose -f docker/docker-compose.yaml up --build -d
+
+# run with log attached to the console
+#docker compose -f docker/docker-compose.yaml up --build
